2459cf1347de3f8eaa7f89c6a8870e9d627af233
Nishant <nishant3@mail.usf.edu>
2019-05-19 22:18:30 -0400

    DYNAMIC PROGRAMMING, 198

---
+++ java_snippet/House_Robber.java
@@ -0,0 +1,37 @@
+
+public class House_Robber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] num = {2,7,9,3,1};
+		int money = rob(num);
+		System.out.println(money);
+	}
+
+	private static int rob(int[] nums) {
+	      if(nums.length==0 || nums==null){
+	            return 0;
+	        }
+	        
+	        if(nums.length==1){
+	            return nums[0];
+	        }
+	        
+	        if(nums.length==2){
+	            return Math.max(nums[0],nums[1]);
+	        }
+	        
+	        int[] dp = new int[nums.length];
+	        dp[0] = nums[0];
+	        dp[1] = Math.max(nums[0],nums[1]);
+	        
+	        
+	        for(int i=2;i<dp.length;i++){
+	            dp[i] = Math.max(nums[i]+dp[i-2],dp[i-1]);
+	        }
+	        
+	        return dp[dp.length-1];
+	}
+
+}
